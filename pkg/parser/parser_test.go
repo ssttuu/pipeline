450f4c83a1d35e815d4bafe0ac37e863359eebf1
Stu Schwartz <stupschwartz@gmail.com>
2018-12-23 08:40:34 -0500

    parse let statements

---
+++ pkg/parser/parser_test.go
@@ -0,0 +1,45 @@
+package parser
+
+import (
+	"github.com/ssttuu/monkey/pkg/ast"
+	"github.com/ssttuu/monkey/pkg/lexer"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestLetStatements(t *testing.T) {
+	input := `
+let x = 5;
+let y = 10;
+let foobar = 838383;
+`
+
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+	if program == nil {
+		t.Fatalf("ParseProgram() returned nil")
+	}
+
+	assert.Len(t, program.Statements, 3)
+
+	tests := []struct {
+		expectedIdentifier string
+	}{
+		{"x"},
+		{"y"},
+		{"foobar"},
+	}
+
+	for i, test := range tests {
+		stmt := program.Statements[i]
+		if !assert.Len(t, p.Errors(), 0) {
+			continue
+		}
+		assert.Equal(t, "let", stmt.TokenLiteral())
+		letStmt, ok := stmt.(*ast.LetStatement)
+		assert.True(t, ok)
+		assert.Equal(t, test.expectedIdentifier, letStmt.Name.Value)
+		assert.Equal(t, test.expectedIdentifier, letStmt.Name.TokenLiteral())
+	}
+}
